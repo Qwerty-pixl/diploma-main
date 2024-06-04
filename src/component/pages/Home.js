f58ccf756c9e8a273ba8e80594e570bc616e6af2
klaratokonova11 <148875652+Qwerty-pixl@users.noreply.github.com>
2024-06-04 22:09:55 +0600

    sdfb

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -5,17 +5,17 @@ import "./Home.css";
 function Home() {
   const categories = [
     {
-      name: 'Dried fruits',
+      name: ' For women',
       products: [
-        { id: 1, name: 'Dried apricots', image: 'https://i1.stat01.com/2/3752/137515038/afacdb/kuraga-quot-korolevskaya-quot-vysshij-sort.jpg', price: '108som' },
-        { id: 2, name: 'Figs', image: 'https://firebasestorage.googleapis.com/v0/b/diplom-597c7.appspot.com/o/products%2F157bf8c7-130d-4ff7-a128-5d387d8fce3f.jpg?alt=media&token=31b0aea8-99d2-4675-bf61-42e807b08dc8', price: '148som' },
+        { id: 1, name: 'Chanel 5', img: 'https://aura-cosmetics.kg/wp-content/uploads/2020/10/CHANEL-%E2%84%965-100ml.jpg', price: '108som' },
+        { id: 2, name: 'Red', img: 'https://fraguru.com/mdimg/perfume/375x500.51422.jpg', price: '148som' },
       ],
     },
     {
-      name: 'Fruits',
+      name: 'For men',
       products: [
-        { id: 3, name: 'Watermelon', image: 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/11/watermelon-1296x728-header-1.jpg?w=1155&h=1528', price: '212som' },
-        { id: 4, name: 'Mandarin', image: 'https://soap-formula.ru/assets/images/products/261/532x315/mandarin.jpg', price: '40 som' },
+        { id: 3, name: 'Bleu De Chanel', img: 'https://aura-cosmetics.kg/wp-content/uploads/2020/10/%D0%A2%D1%83%D0%B0%D0%BB%D0%B5%D1%82%D0%BD%D0%B0%D1%8F-%D0%B2%D0%BE%D0%B4%D0%B0-BLEU-DE-CHANEL.jpg', price: '212som' },
+        { id: 4, name: 'Colognes', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR4v_a1toexE9inBtrX_yZutVWOh6rB-Tdrw&s', price: '40 som' },
       ],
     },
     // Другие категории продуктов
@@ -23,7 +23,7 @@ function Home() {
 
   return (
     <div className="home-page">
-      <h1>Welcome to Markom Store!</h1>
+      <h1> OUR PERFUMES FOR ALL </h1>
       <div className="categories">
         {categories.map((category) => (
           <ProductCategory key={category.name} category={category} />
